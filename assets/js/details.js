5fc7f502e7a4dbbd658da909406a8cbd120fe2f4
DeveloperIsaque <i27052024@gmail.com>
2024-08-06 12:56:51 -0300

    atualizando mudanças

--- assets/js/details.js
+++ assets/js/details.js
@@ -1,6 +1,17 @@
-const opa = 'ditto';
-const abacate = `https://pokeapi.co/api/v2/pokemon/${opa}`;
+const name = 'ditto'
+const url2 = `https://pokeapi.co/api/v2/pokemon/${name}`
 
-function tentativa() {
-    console.log(abacate);
+function convertPokemontoDetails(pokemon) {
+    return `
+    
+    `
 }
+
+fetch(url2)
+    .then((response) => response.json())
+    .then((pokemonDetails) => {
+        console.log(pokemonDetails)
+    })
+    .catch((error) => console.log(error)) 
+
+console.log('sucesso!')
--- assets/js/main.js
+++ assets/js/main.js
@@ -48,4 +48,3 @@ loadMoreButton.addEventListener('click', () => {
     }
 })
 
-tentativa()
